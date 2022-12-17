c94159b315124a73952c6e23e3dcfc0d1b03aa6d
crenaz <crenaz@gmail.com>
2022-12-17 16:41:33 -0400

    added things

---
+++ src/config.ts
@@ -0,0 +1,49 @@
+export const NAV_ITEMS = [
+  {
+    path: '/',
+    title: 'Solid State',
+  },
+  {
+    path: '/generic',
+    title: 'Generic Page',
+  },
+  {
+    path: '/elements',
+    title: 'Elements Reference',
+  },
+];
+
+export const SOCIAL_MEDIA_ITEMS = [
+  {
+    path: '#',
+    title: 'Twitter',
+    iconClass: 'fa-twitter',
+  },
+  {
+    path: '#',
+    title: 'Facebook',
+    iconClass: 'fa-facebook-f',
+  },
+  {
+    path: '#',
+    title: 'Instagram',
+    iconClass: 'fa-instagram',
+  },
+  {
+    path: '#',
+    title: 'GitHub',
+    iconClass: 'fa-github',
+  },
+];
+
+export const SITE = {
+  title: 'This is Solid State',
+  description: 'A free, fully responsive HTML5 + CSS3 site template, ported to Astro',
+};
+
+export const CONTACT = {
+  address: ['1234 Somewhere Road #87257 Nashville', 'TN 00000-0000'],
+  phone: '(000) 000-0000',
+  email: 'info@untitled.tld',
+  formSubmitUrl: '/',
+};
